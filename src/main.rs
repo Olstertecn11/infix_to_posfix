2fd39d27e2126da7444583f16f2c360a21dc66bc
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-16 07:26:16 -0600

    lol

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn main() {
     let operators: String = String::from("+-*/^");
     let input = utils::readline("Enter a expression: ".to_string());
     for i in input.chars(){
-        println!("{}", i);
+        if is_op(i, operators) {
+            println!("is op");
+        }
     }
 
 }
