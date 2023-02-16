a8b87c1fa4da705df1a114be33d98c94f4e5368f
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-16 15:28:58 -0600

    read_number deleted

--- src/utils.rs
+++ src/utils.rs
@@ -24,20 +24,5 @@ pub fn pause(){
     let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
-pub fn read_number()-> i32{
-    let mut number = String::new();
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to read from stdin");
-
-
-    let trimmed = number.trim();
-    let aint: i32 = trimmed.trim().parse().ok().expect("Error in user input");
-    match trimmed.parse ::<i32>(){
-        Ok(_i)=> println!(""),
-        Err(..)=> println!("This was not an integer: {}", number),
-    };
-    return aint;
-}
 
 
